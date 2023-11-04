676c10e584b757b4c56a6c298d4a9714a1973177
hs-moore <heather.sm555@gmail.com>
2023-11-04 17:25:28 +0000

    found the button

--- scrape_models.py
+++ scrape_models.py
@@ -2,13 +2,37 @@ from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 def get_file(url):
     driver = webdriver.Chrome()
     driver.get(url)
-    download_button = driver.find_element(By.ID, 'Download Free 3D Model')
+    cookies = driver.find_element(By.ID, "onetrust-reject-all-handler")
+    cookies.click()
+    title_text = "Download Free 3D Model"
+    download_button = driver.find_element(By.XPATH, f"//button[@title='{title_text}']")
     download_button.click()
+    user = 'email'
+    username = driver.find_element(By.XPATH, f"//input[@type='{user}']")
+    
+    if username:
+        email = "heather.sm555@gmail.com"
+        pw = "Password123!"
+        key = 'password'
+        password = driver.find_element(By.XPATH, f"//input[@type='{key}']")
+        ActionChains(driver)\
+            .send_keys_to_element(username, email)\
+            .perform()
+        ActionChains(driver)\
+            .send_keys_to_element(password, pw)\
+            .perform()
+
+    obj = "jv075PB9"
+    objelement = driver.find_element(By.XPATH, f"//div[@class='{obj}']")
+    objbutton = objelement.find_element(By.CLASS_NAME, "button btn-primary btn-small button-source")
+    objbutton.click()
     driver.quit()
 
 # Get the page

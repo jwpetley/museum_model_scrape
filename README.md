3aac52b520c55d4ea85efb2f12afc451b26b7d92
jwpetley <jwpetley@gmail.com>
2023-11-04 22:00:34 +0000

    Can scrape whole smithsonian

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Museum Scraping Code
 
-
 Code to scrape 3d models from museums and upload to google cloud
--- smithsonian_scrape.py
+++ smithsonian_scrape.py
@@ -15,31 +15,40 @@ def get_collections(url):
     # Parse the page
     soup = BeautifulSoup(page.text, 'html.parser')
 
-    thumbs = soup.find_all("span", class_= "b-text-wrapper")
+    thumbs = soup.find_all("div", class_= "edan-embed-content has-media teaser teaser-long entity")
 
     # Get the model links
-    collections = []
+    collections = {}
     for item in thumbs:
-        collections.append(item.find("span", class_= "title h3").get_text())
+        collection_link = item.find("a", class_ = "inner").get("href")
+        collections[item.find("span", class_= "title h3").get_text()] = collection_link 
 
 
     return collections
 
 def get_pieces(collection):
-    collection = collection.lower()
-    url = f"https://3d.si.edu/collections/{collection}"
+    base_url = "https://3d.si.edu/"
+    url = f"https://3d.si.edu/{collections[collection]}"
+    print(url)
     page = requests.get(url)
+
 
     # Parse the page
     soup = BeautifulSoup(page.text, 'html.parser')
 
-
-    thumbs = soup.find_all("div", class_= "teaser teaser-long")
     links = []
-    for thumb in thumbs:
-        link_elements = thumb.find('a')
-        link = link_elements.get('href')
-        links.append("https://3d.si.edu"+link)
+    while soup.find("li", class_ = "pager-next") is not None:
+        next_page = soup.find("li", class_ = "pager-next").find("a").get("href")
+        page = requests.get(base_url + next_page)
+        soup = BeautifulSoup(page.text, 'html.parser')
+
+
+        thumbs = soup.find_all("div", class_= "teaser teaser-long")
+    
+        for thumb in thumbs:
+            link_elements = thumb.find('a')
+            link = link_elements.get('href')
+            links.append("https://3d.si.edu"+link)
 
     return links
 
@@ -73,25 +82,62 @@ def download_wait(directory, timeout, nfiles=None):
         seconds += 1
     return seconds
 
-def get_model(piece_link):
+def get_model(piece_link, download_dir):
+
+    options = Options()
+    options.add_experimental_option('prefs',  {
+    "download.default_directory": download_dir,
+    "download.prompt_for_download": False,
+    "download.directory_upgrade": True,
+    "profile.managed_default_content_settings.images": 2,
+    }   
+    )
+
+    WINDOW_SIZE = "1920,1080"
+
+    options.add_argument("--headless")
+    options.add_argument("--window-size=%s" % WINDOW_SIZE)
+    
+
     driver = webdriver.Chrome(options = options)
     driver.get(piece_link)
     text = "heading-tab-download"
     download_button = driver.find_element(By.XPATH, f"//button[@id='{text}']")
     download_button.click()
-
+    
 
     try:
-        file_type = "\"150k-4096-obj_std.zip\""
+        try:
+            file_type = "\"Low Resolution 3D Mesh, OBJ\""
+            usdz_file = driver.find_element(By.XPATH, 
+                                            f"//a[contains(., {file_type})]")
+        
+        except:
+            try:
+                file_type = "\"Low Resolution 3D mesh, obj\""
+                usdz_file = driver.find_element(By.XPATH, 
+                                            f"//a[contains(., {file_type})]")
+            except:
+                file_type = "\"Low resolution 3D mesh, obj\""
+                usdz_file = driver.find_element(By.XPATH, 
+                                            f"//a[contains(., {file_type})]")
 
-        usdz_file = driver.find_element(By.XPATH, f"//a[contains(., {file_type})]")
+
+        file_name = usdz_file.get_attribute("href").split("/")[-1]
+
+       
+        if file_name in os.listdir(download_dir):
+            print("File already exists.")
+            return None
+        
+        driver.execute_script("arguments[0].scrollIntoView();", usdz_file)
         usdz_file.click()
 
         seconds = download_wait(download_dir, 300)
 
         print("Download took {} seconds.".format(seconds))
     except: 
-        "Cannot download .usdz for this model."
+        pass
 
     driver.quit()
 
@@ -111,29 +157,38 @@ def push_to_cloud(folder):
 
 if __name__ == "__main__":
 
-    download_dir = "/home/qjfn45/Documents/museum_model_scrape/models"
+    download_dir_base = "/home/qjfn45/Documents/museum_model_scrape/models"
 
     options = Options()
     options.add_experimental_option('prefs',  {
-    "download.default_directory": download_dir,
+    "download.default_directory": download_dir_base,
     "download.prompt_for_download": False,
     "download.directory_upgrade": True,
     "profile.managed_default_content_settings.images": 2,
     }   
     )
 
+
     url = "https://3d.si.edu/collections"
 
     collections = get_collections(url)
+    print(collections)
+
+    for collection in collections:
+        pieces = get_pieces(collection)
+        print(collection, pieces)
+        download_dir = download_dir_base + "/" + collection
+        try:
+            os.mkdir(download_dir)
+        except:
+            print("Directory already exists.")
 
 
-    pieces = get_pieces(collections[0])
+        for piece in pieces:
+            get_model(piece, download_dir)
+            print("Downloaded model {} from collection {}.".format(piece, collection))
 
-
-    #for piece in pieces:
-    #    get_model(piece)
-
-    push_to_cloud(download_dir)
+    #push_to_cloud(download_dir)
 
 
 
